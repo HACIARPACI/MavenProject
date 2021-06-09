a6abb37421d93d1feebbd829f026745751aa3c24
haciarpaci <haciarpaci12@gmail.com>
2021-06-09 10:11:26 +0300

    added more hello feature

--- src/main/java/Hello.java
+++ src/main/java/Hello.java
@@ -3,6 +3,8 @@ public class Hello {
     public static void main(String[] args) {
         System.out.println("hello");
         System.out.println("developing hello feature");
+        System.out.println("Local repo  is already published to Github");
+        System.out.println("doin more wore");
     }
 
 
